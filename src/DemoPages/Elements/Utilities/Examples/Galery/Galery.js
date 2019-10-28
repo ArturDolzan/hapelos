21fa3086a39c75e08d7a5c145fb4e13c85e5675d
AlessandroJr <ale98nunes98@gmail.com>
2019-10-28 13:06:39 -0300

    Implementacao

--- src/DemoPages/Elements/Utilities/Examples/Galery/Galery.js
+++ src/DemoPages/Elements/Utilities/Examples/Galery/Galery.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {
     Col,
     Card, CardBody,
@@ -11,47 +11,35 @@ import {Link} from 'react-router-dom'
 class Galery extends React.Component {
     render() {
         return (
-            <Col md="3">
-                <Card className="card-lista-produtos">
-                    <div className="demo-image-bg">
-                        <img src={`data:image/jpeg;base64,${this.props.Produto.foto}`}/>
-                    </div>
-                    <CardBody>
-                        
-                        <CardTitle>
-                            {this.props.Produto.nome}
-                        </CardTitle>
-                        <CardSubtitle className="sub-produto">
-                            {this.props.Produto.descricao}
-                        </CardSubtitle>
+            <Fragment>
+                <Col md="3">
 
-                        <ul>
-                            {this.props.Produto.tamanho && (
-                                <li><b>Tamanho: </b>
-                                    {this.props.Produto.tamanho}
-                                </li>
-                            )}
-                            {this.props.Produto.dimensoes && (
-                                <li><b>Dimensão: </b>
-                                    {this.props.Produto.dimensoes}
-                                </li>
-                            )}
-                            {this.props.Produto.cor && (
-                                <li><b>Cor: </b>
-                                    {this.props.Produto.cor}
-                                </li>
-                            )}
-                            <li><b>Preço: </b>
-                                {this.props.Produto.preco}
-                            </li>
-                        </ul>
-                        
-                        <Link to={'/shopitem/' + this.props.Produto.id}>
-                             <Button className="mb-2 mr-2" color="success">Comprar</Button>
-                        </Link>
-                    </CardBody>
-                </Card>
-            </Col>
+                    <Card className="card-lista-produtos">
+                        <div className="card-item">
+                            <img src={`data:image/jpeg;base64,${this.props.Produto.foto}`}/>
+                        </div>
+                        <CardBody>
+                            
+                            <CardTitle>
+                                {this.props.Produto.nome}
+                            </CardTitle>
+                            <CardSubtitle className="sub-produto">
+                                {this.props.Produto.descricao}
+                            </CardSubtitle>
+
+                            <Link to={ '/shopitem/' + this.props.Produto.id}> 
+                                <Button className="mb-2 mr-2 card-item-button" color="success">Comprar</Button>
+                            </Link>
+
+                            <Link to={'/shopitem/' + this.props.Produto.id}>
+                                <Button className="mb-2 mr-2 card-cart-button" color="info"><i className="pe-7s-cart"/></Button>
+                            </Link>
+
+                        </CardBody>
+                    </Card>
+                    
+                </Col>
+            </Fragment>
         )
     }
 }
--- src/DemoPages/ShopItem/index.js
+++ src/DemoPages/ShopItem/index.js
@@ -26,68 +26,80 @@ const Produtos = {
     Desconto: 'Desconto'
 };
 
-const ShopItem = ({match}) => (
-    <Fragment>
-        <AppHeader/>
-        <PageTitle
-            style={{
-                marginTop: "60px!important"
-            }}
-            heading="Filtros"
-            subheading=""
-            icon="pe-7s-filter icon-gradient bg-deep-blue"
-        />
-        <div className="app-main">
-            <AppSidebar/>
-            
-            <div className="app-main__outer">
-                <div className="app-main__inner">
-                    <Row>
-                        <Col md="5" className="itemDetallhe">
-                            <div className="demo-image-bg" 
-                                style = {{
-                                    backgroundImage: 'url(' + Produtos.Imagem + ')',
-                                    width: '100%',
-                                    height: '100%'
-                                }}
-                            />
-                        </Col>
-                        <Col md="5" className="itemDetallhe">
-                            <h1>
-                                Nome do Item
-                            </h1>
-                            <ul className="itemDetalheLista">
-                                {Produtos.Tamanho && (
-                                    <li><b>Tamanho: </b>
-                                        {Produtos.Tamanho}
-                                    </li>
-                                )}
-                                {Produtos.Dimensao && (
-                                    <li><b>Dimensão: </b>
-                                        {Produtos.Dimensao}
-                                    </li>
-                                )}
-                                {Produtos.Cor && (
-                                    <li><b>Cor: </b>
-                                        {Produtos.Cor}
-                                    </li>
-                                )}
-                                <li><b>Preço: </b>
-                                    {Produtos.Preco}
-                                </li>
-                            </ul>
+class ShopItem extends React.Component{
 
-                            <Button className="mb-2 mr-2" color="success">Adicionar ao carrinho</Button>                            
-                            <Button className="mb-2 mr-2" color="success">Comprar</Button>                            
-                        </Col>
-                    </Row>
+    componentDidMount() {
+
+        //IMplementar chamada no backend para carregar o item passando o ID que vem por parametro
+    }
+
+    render(){
+
+        return (
+            <Fragment>
+                
+                <AppHeader/>
+                <PageTitle
+                    style={{
+                        marginTop: "60px!important"
+                    }}
+                    heading="Filtros"
+                    subheading=""
+                    icon="pe-7s-filter icon-gradient bg-deep-blue"
+                />
+                <div className="app-main">
+                    <AppSidebar/>
+                    
+                    <div className="app-main__outer">
+                        <div className="app-main__inner">
+                            <Row>
+                                <Col md="5" className="itemDetallhe">
+                                    <div className="demo-image-bg" 
+                                        style = {{
+                                            backgroundImage: 'url(' + Produtos.Imagem + ')',
+                                            width: '100%',
+                                            height: '100%'
+                                        }}
+                                    />
+                                </Col>
+                                <Col md="5" className="itemDetallhe">
+                                    <h1>
+                                        {/* {JSON.parse(props.Produto.Nome)} */}
+                                    </h1>
+                                    <ul className="itemDetalheLista">
+                                        {Produtos.Tamanho && (
+                                            <li><b>Tamanho: </b>
+                                                {Produtos.Tamanho}
+                                            </li>
+                                        )}
+                                        {Produtos.Dimensao && (
+                                            <li><b>Dimensão: </b>
+                                                {Produtos.Dimensao}
+                                            </li>
+                                        )}
+                                        {Produtos.Cor && (
+                                            <li><b>Cor: </b>
+                                                {Produtos.Cor}
+                                            </li>
+                                        )}
+                                        <li><b>Preço: </b>
+                                            {Produtos.Preco}
+                                        </li>
+                                    </ul>
+        
+                                    <Button className="mb-2 mr-2" color="success">Adicionar ao carrinho</Button>                            
+                                    <Button className="mb-2 mr-2" color="success">Comprar</Button>                            
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+        
+                   
                 </div>
-            </div>
-
-           
-        </div>
-        <AppFooter></AppFooter>
-    </Fragment>
-);
+                <AppFooter></AppFooter>
+            </Fragment>
+        )
+    }
+} 
 
 export default ShopItem;
