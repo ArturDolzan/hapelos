67feeeed641bbc47e46b64259adc3b311374d56d
AlessandroJr <ale98nunes98@gmail.com>
2019-11-04 20:40:45 -0300

    envio de e-mails

--- src/DemoPages/Login/index.js
+++ src/DemoPages/Login/index.js
@@ -1,7 +1,9 @@
 import React, {Fragment} from 'react'
 import { Link } from 'react-router-dom'
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap"
-import PageTitle from 'Layout/AppMain/PageTitle';
+import PageTitle from 'Layout/AppMain/PageTitle'
+import axios from 'axios'
+import {connect} from 'react-redux'
 
 // Layout
 
@@ -11,30 +13,60 @@ import AppFooter from '../../Layout/AppFooter/';
 
 class Login extends React.Component{
 
-    // constructor(props) {
-    //     super(props);
+    constructor(props) {
+        super(props);
 
-    //     this.state = {
-    //         email: "",
-    //         password: ""
-    //     }
-    // }
+        this.state = {
+            email: "",
+            nome: "",
+            telefone: ""
+        }
+    }
 
-    // validateForm() {
-    //     return this.state.email.length > 0 && this.state.password.length > 0;
-    // }
+     validateForm() {
+         return this.state.email.length > 0 && this.state.nome.length > 0 && this.state.telefone.length > 0;
+     }
     
-    // handleChange = event => {
-    //     this.setState({
-    //         [event.target.id]: event.target.value
-    //     });
-    // }
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
 
-    // handleSubmit = event => {
-    //     event.preventDefault();
+    handleSubmit = event => {
+        event.preventDefault();
 
-    //     this.props.history.push('/');
-    // }
+        let produtos = []
+
+        this.props.carrinho.map((item, idx) => {
+
+            produtos.push(
+                {
+                    id: item.id,
+                    nome: item.nome,
+                    qtde: item.qtde
+                }
+            )
+        })
+
+        axios.post(`https://www.infisio.com.br/apihapelos/email/confirmacaoCompra`,
+        {
+            destinatario: this.state.email,
+            nome: this.state.nome,
+            telefone: this.state.telefone,
+            produtos
+        })
+        .then(_ => {
+            
+            alert('Compra finalizada com sucesso!')
+
+            this.props.history.push('/')
+        })
+        .catch(error => {
+            alert(`Erro ao finalizar compra. Erro: ${error}`)
+        })
+
+    }
 
     render(){
 
@@ -44,41 +76,36 @@ class Login extends React.Component{
                 <AppHeader/>
 
                 <div className="Login">
-                    <form>
-                        <FormGroup controlId="name" bsSize="large">
+                    <form onSubmit={this.handleSubmit}>
+                        <FormGroup controlId="nome" bsSize="large">
                             <ControlLabel>Nome</ControlLabel>
                             <FormControl
                             autoFocus
                             type="text"
-                            // value={this.state.email}
-                            // onChange={this.handleChange}
+                            value={this.state.nome}
+                            onChange={this.handleChange}
                             />
                         </FormGroup>
                         <FormGroup controlId="email" bsSize="large">
                             <ControlLabel>E-mail</ControlLabel>
                             <FormControl
-                            // value={this.state.password}
-                            // onChange={this.handleChange}
+                             value={this.state.email}
+                             onChange={this.handleChange}
                             type="email"
                             />
                         </FormGroup>
-                        {/* <FormGroup controlId="password" bsSize="large">
-                            <ControlLabel>Endereço</ControlLabel>
-                            <FormControl
-                            // value={this.state.password}
-                            // onChange={this.handleChange}
-                            type="text"
-                            />
-                        </FormGroup> */}
-                        <FormGroup controlId="phone" bsSize="large">
+                       
+                        <FormGroup controlId="telefone" bsSize="large">
                             <ControlLabel>Telefone</ControlLabel>
                             <FormControl
+                             value={this.state.telefone}
+                             onChange={this.handleChange}
                             />
                         </FormGroup>
                         <Button
                             block
                             bsSize="large"
-                            // disabled={!this.validateForm()}
+                             disabled={!this.validateForm()}
                             type="submit"
                             style= {{
                                 backgroundColor: '#17a2b8',
@@ -89,14 +116,6 @@ class Login extends React.Component{
                             Confirmar
                         </Button>
 
-
-                        {/* <Link className="d-flex justify-content-center" to={`/login/registrar`} 
-                            style={{
-                                color: '#17a2b8'
-                            }}
-                        >
-                            Registrar */}
-                        {/* </Link> */}
                     </form>
                 </div>
                    
@@ -106,4 +125,11 @@ class Login extends React.Component{
     }
 } 
 
-export default Login;
+
+const mapStateToProps = state => ({
+    carrinho: state.CarrinhoReducer.itensCarrinho
+});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
